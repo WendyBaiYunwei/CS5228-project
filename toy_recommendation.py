9be539082ad84cd8c514bccb98ca8c5642d9c736
whitesnowhahorha <yunweibai@gmail.com>
2021-10-23 16:39:20 +0800

    add in filter by category

--- toy_recommendation.py
+++ toy_recommendation.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import TfidfTransformer,CountVectorizer
 import csv
 import re
 
+R_SIZE = 5 #items to recommend through each method
 
 df = pd.read_csv('numerical_cleaned.csv')
 
@@ -43,17 +44,21 @@ tfidf_transformer.fit(word_cnt)
 tfidfs=tfidf_transformer.transform(cv.transform(df['soup']))
 query="A big comfortable car with 1 owner and 320i gt m-sports model"
 
-def get_similar(base_result_i):
-    useful_features = df[['price', 'mileage']]
+def get_similar(base_result, base_result_i):
+    useful_features = df[['price', 'age', 'mileage']]
     base_result_features = useful_features.iloc[base_result_i.index.to_list()]
     pair_wise_sim = cosine_similarity(useful_features, base_result_features)
     w_price = 5
+    w_age = 3
     w_mileage = 3
     df['similarity_num']=pair_wise_sim[:, 0] * w_price  +\
-            pair_wise_sim[:, 2] * w_mileage
+        pair_wise_sim[:, 1] * w_age + \
+            pair_wise_sim[:, 4] * w_mileage
     res=df.sort_values(by=['similarity_num', 'similarity'],ascending=False)
-
-    return res[:5]
+    valid_types = list(base_result['type_of_vehicle'].values)
+    is_valid = res.type_of_vehicle.isin(valid_types)
+    res=res[is_valid]
+    return res[:R_SIZE]
 
 def get_base_recommendation(query):
     print(cv.transform([query]))
@@ -66,10 +71,10 @@ def get_base_recommendation(query):
     base_result=df.sort_values(by=['similarity'],ascending=False)
     base_result_i=df.sort_values(by=['similarity'],ascending=False)
 
-    return (base_result[:5], base_result_i[:5])
+    return (base_result[:R_SIZE], base_result_i[:R_SIZE])
 
 base_result, base_result_i=get_base_recommendation(query)
-extended = get_similar(base_result_i)
+extended = get_similar(base_result, base_result_i)
 
 res = [base_result, extended]
 res = pd.concat(res)
