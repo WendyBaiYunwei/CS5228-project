ab5911aee3bf20bde217277b70d33bbcb3bc715d
whitesnowhahorha <yunweibai@gmail.com>
2021-11-04 17:32:39 +0800

    select columns for training

--- toy_test.py
+++ toy_test.py
@@ -1,23 +1,20 @@
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import GridSearchCV
 import pandas as pd
-from pprint import pprint
 
-X_train = pd.read_csv('small_train.csv')
+l = pd.read_csv('small_test.csv').columns.tolist()
+X_train = pd.read_csv('small_train.csv')[l]
 X_test = pd.read_csv('small_test.csv')
-y_train = pd.read_csv('small_train_y.csv')
-y_test = pd.read_csv('small_test_y.csv')
+y_train = pd.read_csv('small_train_y.csv')['price']
+y_test = pd.read_csv('small_test_y.csv')['price']
 
-
-# parameters = {'n_estimators': [150, 250]}
 model = RandomForestRegressor(n_estimators = 700, max_features = 7)
-# model = GridSearchCV(clf, parameters)
+
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
-# best_params = model.best_params_
 mse=mean_squared_error(y_test, y_pred)
 print(mse)
 
